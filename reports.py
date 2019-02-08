d523b17ef0c0c46d46608ee404495c5a71c104a2
Tamas Floch <tamasfloch95@gmail.com>
2019-02-08 13:22:17 +0100

    asd

--- reports.py
+++ reports.py
@@ -1,2 +1,14 @@
 
 # Report functions
+
+
+def get_selling_avg(file_name):
+    with open(file_name, 'r') as f:
+        games= 0
+        sold_copies = float(0)
+        for line in f:
+            line = line.split("\t")
+            games += 1
+            sold_copies += float(line[1])
+        output = sold_copies / float(games)
+    return output
