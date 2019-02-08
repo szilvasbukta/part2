46628753cdcf74391642e4f888ad979c6ba4e747
matyasczuczor <matyas.czuczor@gmail.com>
2019-02-08 13:33:32 +0100

    first function implemented

--- reports.py
+++ reports.py
@@ -1,2 +1,13 @@
 
 # Report functions
+
+
+def get_most_played(file_name):
+    with open(file_name, 'r') as opened_file:
+        most_copies = ['', 0]   # game title, copies_sold
+        for line in opened_file:
+            line = line.split("\t")
+            if float(line[1]) > float(most_copies[1]):
+                most_copies[1] = line[1]
+                most_copies[0] = line[0]
+        return most_copies[0]
