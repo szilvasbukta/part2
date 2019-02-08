ad2f2ed86dbbdf89d227e3d6e295c323c225eb1e
krekkPatrik <krekk.patrik@gmail.com>
2019-02-08 13:13:46 +0100

    done with 2nd func.

--- reports.py
+++ reports.py
@@ -1,2 +1,11 @@
 
 # Report functions
+
+
+def sum_sold(file_name):
+    count = float(0)
+    with open(file_name, 'r') as f:
+        for line in f:
+            game_info = line.split("\t")
+            count += float(game_info[1])
+    return count
